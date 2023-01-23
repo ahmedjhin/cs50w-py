df9b5da4e993037f848ed12ffc871562d5a76862
ahmedjhin <a7madti90@hotmail.com>
2023-01-23 21:48:35 +0400

    add

---
+++ flight.py
@@ -0,0 +1,26 @@
+class flight:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+    def addPassengers(self, name):
+        if  not self.openseats():
+            return False
+        self.passengers.append(name)
+        return True
+    
+    def openseats(self):
+        return self.capacity - len(self.passengers)
+    
+pepole = ["ali", "naser" ,"ahmed"]
+point = flight(2)
+
+
+
+for person in pepole:
+    sucss = point.addPassengers(person)
+    if  sucss:
+         print(f"added {person}")
+    else:
+        print(f"not add {person}")
+        
